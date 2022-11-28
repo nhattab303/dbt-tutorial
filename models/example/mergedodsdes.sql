88885b727c042ca3f5feb7a0ad7ba4d14e9d282f
Nizar Hattab <nhattab@reconess.com>
2022-11-28 15:49:30 +0200

    is_incr

--- models/example/mergedodsdes.sql
+++ models/example/mergedodsdes.sql
@@ -1,8 +1,17 @@
 
 {{ config(materialized='incremental', schema='EXTRACTOR_SERVICES')}}
 
+# if incremental
 
 select *
 from {{ ref('desjpposts') }}
 join {{ ref('odsalljobs') }}
 using (job_id)
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  where event_time > (select max(event_time) from {{ this }})
+
+{% endif %}
+
+
