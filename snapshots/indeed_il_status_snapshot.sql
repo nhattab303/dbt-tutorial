78856642a4123bc642779f9752625bf120055d7f
Nizar Hattab <nizar.hattab@retrain.ai>
2023-01-11 17:09:25 +0200

    testing snapshot

--- snapshots/indeed_il_status_snapshot.sql
+++ snapshots/indeed_il_status_snapshot.sql
@@ -1,15 +1,20 @@
-{% snapshot ods_indeed_job_post_status %}
+
+{% snapshot ods_indeed_job_post_status_snapshot %}
+
+{% set source_name='ODS_INDEED_STATUS' %}
+{% set source_table= 'ODS_INDEED_' + var("site")|string|upper + '_STATUS_TEST2' %}
+
 
 {{
     config(
       target_database='DEV_DB',
-      target_schema='snapshots',
+      target_schema='ODS',
       unique_key='job_id',
       strategy='timestamp',
       updated_at='time_updated',
     )
 }}
 
-select * from {{ ref('ODS_INDEED_IL_STATUS_TEST1') }}
+select * from {{ source(source_name, source_table) }}
 
 {% endsnapshot %}
